496263954d3f74d2313aebe970f92e60ac8e20e5
alex <alex_is_ok@yahoo.com>
2019-08-19 15:36:18 +0300

    twig filter rate added

--- src/Twig/AppExtention.php
+++ src/Twig/AppExtention.php
@@ -2,16 +2,34 @@
 
 namespace App\Twig;
 
+use App\Repository\PostRepository;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 use App\Repository\ReactionRepository;
 
 class AppExtention extends AbstractExtension
 {
+    private $postRepository;
+    private $reactionRepository;
+
+    public function __construct(PostRepository $repository, ReactionRepository $reactionRepository)
+    {
+        $this->postRepository = $repository;
+        $this->reactionRepository = $reactionRepository;
+    }
+
     public function getFunctions()
     {
         return [
             new TwigFunction('reactions', [$this, 'calculateReactions']),
+        ];
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('rate', [$this, 'ratePostByPositiveReactions']),
         ];
     }
 
@@ -22,5 +40,23 @@ class AppExtention extends AbstractExtension
         } else {
             return $reactionRepository->countPostReactionsById($postId);
         }
+    }
+
+    public function ratePostByPositiveReactions($posts)
+    {
+        $result = [];
+        $res = [];
+        foreach ($posts as $p) {
+            $res[$p->getId()] =
+                $this->reactionRepository->countPostReactionsByLegend('good', $p->getId()) +
+                $this->reactionRepository->countPostReactionsByLegend('very good', $p->getId());
+        }
+        arsort($res);
+        array_reverse($res);
+
+        foreach (array_keys($res) as $r) {
+            array_push($result, $this->postRepository->find($r));
+        }
+        return $result;
     }
 }
